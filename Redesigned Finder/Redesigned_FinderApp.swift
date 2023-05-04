cde1fb9c6a7b40edf96e6557cdae335b17681a49
Garrit Witters <garritwitters2@gmail.com>
2023-05-04 19:42:51 -0400

    Initial Commit

---
+++ Redesigned Finder/Redesigned_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Redesigned_FinderApp.swift
+//  Redesigned Finder
+//
+//  Created by Garrit Witters on 5/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Redesigned_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
